2b58d41c0fc360c697f653a488bae03272e8de2b
sattip7 <satish.ponnam87@gmail.com>
2014-04-19 17:27:45 +0530

    Landing Controller is added.

---
+++ src/main/java/com/gulludada/harosys/mvc/LandingPageController.java
@@ -0,0 +1,18 @@
+package com.gulludada.harosys.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class LandingPageController {
+	
+	private static String HOME_PAGE = "home";
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String showHomePage() {
+		return HOME_PAGE;
+	}
+}
