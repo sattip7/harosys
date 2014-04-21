171b4534540b62b0fc3d04a18d17e88dde63b698
sattip7 <satish.ponnam87@gmail.com>
2014-04-21 12:42:48 +0530

    UserController

--- src/main/java/com/gulludada/harosys/mvc/LandingPageController.java
+++ src/main/java/com/gulludada/harosys/mvc/LandingPageController.java
@@ -1,4 +1,4 @@
-package com.gulludada.harosys.mvc;
+ package com.gulludada.harosys.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
