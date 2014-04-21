54bb7eaa7919679dd20743f81f6a2f203eab2f6b
sattip7 <satish.ponnam87@gmail.com>
2014-04-21 14:50:04 +0530

    pom and LandingPageController and UserLoginController are changed. add loggers

--- src/main/java/com/gulludada/harosys/user/mvc/LandingPageController.java
+++ src/main/java/com/gulludada/harosys/user/mvc/LandingPageController.java
@@ -1,5 +1,7 @@
- package com.gulludada.harosys.user.mvc;
+package com.gulludada.harosys.user.mvc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,12 +9,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class LandingPageController {
-	//new comment
-	private static String HOME_PAGE = "home";
+
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(LandingPageController.class);
 	
-	
+	private static final String REDIRECTING_TO_USER_LOGIN_PAGE = "user/login";
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String showHomePage() {
-		return HOME_PAGE;
+	public String landingPage(SErvletR) {
+		
+		/*String ipAddress = request.getHeader("X-FORWARDED-FOR");  
+		   if (ipAddress == null) {  
+			   ipAddress = request.getRemoteAddr();  
+		   }*/
+		return REDIRECTING_TO_USER_LOGIN_PAGE;
 	}
 }
--- src/main/java/com/gulludada/harosys/user/mvc/UserLoginController.java
+++ src/main/java/com/gulludada/harosys/user/mvc/UserLoginController.java
@@ -1,5 +1,7 @@
 package com.gulludada.harosys.user.mvc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import com.gulludada.harosys.user.mvc.dto.LdapUserDto;
 @Controller
 public class UserLoginController {
 
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(UserLoginController.class);
 	private static final String LOGIN_PAGE = "login";
 	private static final String SIGN_UP_PAGE = "";
 	private static final String FORGOT_PASSWORD_PAGE = "";
@@ -18,27 +22,35 @@ public class UserLoginController {
 
 	@RequestMapping(value = "user/login", method = RequestMethod.GET)
 	public String showLoginPage(ModelMap map) {
+		LOGGER.debug("================> user login form is requested.");
 		map.addAttribute("ldapUserDto", new LdapUserDto());
 		return LOGIN_PAGE;
 	}
-	
+
 	@RequestMapping(value = "user/login", method = RequestMethod.POST)
 	public String submitLoginPage() {
+		LOGGER.info("================>> A user is is trying to login with username "
+				+ "...");
 		return SUCCESS_LOGIN_PAGE;
 	}
-	
+
 	@RequestMapping(value = "user/forgot", method = RequestMethod.POST)
 	public String getPasswordByForgotPassword() {
+		LOGGER.info("================>> A user is forgoted password with mail or userid "
+				+ "...");
 		return FORGOT_PASSWORD_PAGE;
 	}
-	
+
 	@RequestMapping(value = "user/register", method = RequestMethod.GET)
 	public String showSignUpPage(ModelMap map) {
+		LOGGER.debug("================> user sign form is requested.");
 		return SIGN_UP_PAGE;
 	}
 
 	@RequestMapping(value = "user/register", method = RequestMethod.POST)
 	public String submitSignUpPage() {
+		LOGGER.info("================>> A user is trying to create a a/c with mailid or username "
+				+ "...");
 		return SUCCESS_REGISTER_PAGE;
 	}
 }
